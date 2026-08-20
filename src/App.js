30614a41425f601689c7151522505098946411e5
Raul Castillo <sx353553@yahoo.com>
2026-08-20 13:36:48 -0500

    update code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Discounted from './components/Discounted';
 import Explore from './components/Explore';
 import Featured from  './components/Featured'
+import Footer from './components/Footer';
 import Highlights from './components/Highlights'
 import Landing from './components/Landing';
 import Nav from './components/Nav';
@@ -16,6 +17,7 @@ function App() {
       <Featured />
       <Discounted />
       <Explore />
+      <Footer />
     </div>
   );
 }
