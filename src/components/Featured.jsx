50d15a981866ce433525bc920f4943100a5217af
Raul Castillo <sx353553@yahoo.com>
2026-08-20 06:25:58 -0500

    update code

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -1,7 +1,10 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import React from 'react'
+import React from "react";
+import Book from "./Ui/Book";
+import { books } from "../data.js";
 
 const Featured = () => {
+  console.log(books);
+  console.log();
   return (
     <div>
       <section className="Featured">
@@ -11,35 +14,18 @@ const Featured = () => {
               Featured <span className="purple">Books</span>
             </h2>
             <div className="books">
-              <div className="book">
-                <a href="">
-                  <figure className="book__img--wrapper">
-                    <img src="https://m.media-amazon.com/images/I/61mIq2iJUXL._AC_UF1000,1000_QL80_.jpg" alt="" className="book__img" />
-                  </figure>
-                </a>
-                <div className="book__title">
-                  <a href="/" className='book__title--link'>
-                  Atomic Habits
-                  </a>
-                  <div className="book__rating">
-                  <FontAwesomeIcon icon="star" />
-                  <FontAwesomeIcon icon="star" />
-                  <FontAwesomeIcon icon="star" />
-                  <FontAwesomeIcon icon="star" />
-                  <FontAwesomeIcon icon="star-half-alt" />
-                  </div>
-                  <div className="book__price">
-                    <span className="book__price--normal">$15.00</span>
-                    $10.00
-                  </div>
-                </div>
-              </div>
-            </div>
+              {books
+                .filter((book) => book.rating === 5)
+                .slice(0, 4)
+                .map((book) => (
+                  <Book book={book} key={book.id}/>
+                ))}
+             </div>
           </div>
         </div>
       </section>
     </div>
-  )
-}
+  );
+};
 
-export default Featured
+export default Featured;
