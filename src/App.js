060a0df9713e0b0722ff12caa1f83501504794ed
Raul Castillo <sx353553@yahoo.com>
2026-08-25 15:41:21 -0500

    update code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import Discounted from './components/Discounted';
-import Explore from './components/Explore';
-import Featured from  './components/Featured'
-import Footer from './components/Footer';
-import Highlights from './components/Highlights'
-import Landing from './components/Landing';
-import Nav from './components/Nav';
-
-
+import Footer from "./components/Footer";
+import Nav from "./components/Nav";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      <Nav />
-      <Landing />
-      <Highlights />
-      <Featured />
-      <Discounted />
-      <Explore />
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <Route path="/" exact component={Home} />
+        
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
