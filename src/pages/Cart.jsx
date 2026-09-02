49586088da759559bbfe1f5d647492516656a098
Raul Castillo <sx353553@yahoo.com>
2026-09-02 15:31:53 -0500

    update code

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,22 +1,33 @@
-import React from 'react'
+import React from "react";
 
 const Cart = () => {
   return (
- <div id="book__body">
-  <main id="books__main">
-    <div className="book__container">
-      <div className="row">
-        <div className="book__selected--top">
-          <h2 className="cart__title">Cart</h2>
-         </div>
-         <div className="cart">
-          <div className="cart__header"></div>
-         </div>
-      </div>
+    <div id="books__body">
+      <main id="books__main">
+        <div className="books__container">
+          <div className="row">
+            <div className="book__selected--top">
+              <h2 className="cart__title">Cart</h2>
+            </div>
+            <div className="cart">
+              <div className="cart__header">
+                <span className="cart__book">Book</span>
+                <span className="cart__quantity">Quantity</span>
+                <span className="cart__total">Price</span>
+              </div>
+              <div className="cart__body">
+                <div className="car__item">
+                  <div className="cart__book">
+                    <img src="" className="cart__book--img"alt="" />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
-  </main>
- </div>
-  )
-}
+  );
+};
 
-export default Cart
+export default Cart;
