3aec7d3b9858f25318083d0a963fc7bd6a1c3fdc
Raul Castillo <sx353553@yahoo.com>
2026-09-02 11:34:08 -0500

    update code

--- src/pages/BookInfo.jsx
+++ src/pages/BookInfo.jsx
@@ -1,38 +1,78 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Rating from '../components/Ui/Rating';
-import Price from '../components/Ui/Price';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import { Link, useParams } from "react-router-dom";
+import Rating from "../components/Ui/Rating";
+import Price from "../components/Ui/Price";
+import Book from "../components/Ui/Book";
 
 const BookInfo = ({ books }) => {
+  const { id } = useParams();
+  const book = books.find((b) => b.id === Number(id));
+
+  if (!book) return <p>Book not found</p>;
+
   return (
     <div id="books__body">
       <main id="books__main">
         <div className="books__container">
           <div className="row">
             <div className="book__selected--top">
-             <Link to="/books" className="book__link">
-               <FontAwesomeIcon icon="arrow-left" />
-             </Link>
-             <h2 className="book__selected--title--top">Books</h2>
+              <Link to="/books" className="book__link">
+                <FontAwesomeIcon icon="arrow-left" />
+              </Link>
+              <h2 className="book__selected--title--top">Books</h2>
             </div>
             <div className="book__selected">
               <figure className="book__selected--figure">
-                <img src="https://m.media-amazon.com/images/I/61mIq2iJUXL._AC_UF1000,1000_QL80_.jpg" alt="" className="book__selected--img" />
+                <img src={book.url} alt="" className="book__selected--img" />
               </figure>
               <div className="book__selected--description">
-                <h2 className="book__selected--title">Cracking the Coding Interview</h2>
-                <Rating rating="4.5" />
+                <h2 className="book__selected--title">{book.title}</h2>
+                <Rating rating={book.rating} />
                 <div className="book__selected--price">
-                  <Price originalPrice={50} salePrice={20}/> 
+                  <Price
+                    originalPrice={book.originalPrice}
+                    salePrice={book.salePrice}
+                  />
                 </div>
+                <div className="book__summary">
+                  <h3 className="book__summary--title">Summary</h3>
+                  <p className="book__summary--para">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Aspernatur similique expedita atque nesciunt natus illum,
+                    magnam assumenda? Vitae, eveniet unde nam ut id error magni
+                    quidem consequatur dolorum fuga voluptatum.
+                  </p>
+                  <p className="book__summary--para">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Aspernatur similique expedita atque nesciunt natus illum,
+                    magnam assumenda? Vitae, eveniet unde nam ut id error magni
+                    quidem consequatur dolorum fuga voluptatum.
+                  </p>
+                </div>
+                <button className="btn">Add to cart</button>
               </div>
-            </div> 
+            </div>
+          </div>
+        </div>
+        <div className="book__container">
+          <div className="row">
+            <div className="book__selected--top">
+              <h2 className="book__selected--title--top">Recommended Books</h2>
+            </div>
+            <div className="books">
+            {books
+              .filter((book) => book.rating === 5 && +book.id !== +id)
+              .slice(0, 4)
+              .map((book) => (
+                <Book book={book} key={book.id} />
+              ))}
+              </div>
           </div>
         </div>
       </main>
     </div>
-  )
-}
+  );
+};
 
-export default BookInfo
+export default BookInfo;
---
+++ src/pages/Cart.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Cart = () => {
+  return (
+ <div id="book__body">
+  <main id="books__main">
+    <div className="book__container">
+      <div className="row">
+        <div className="book__selected--top">
+          <h2 className="cart__title">Cart</h2>
+         </div>
+         <div className="cart">
+          <div className="cart__header"></div>
+         </div>
+      </div>
+    </div>
+  </main>
+ </div>
+  )
+}
+
+export default Cart
